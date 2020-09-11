dd829104a0319c58811021ce78822df7e940ac39
Nduhiu17 <nduhiu254@gmail.com>
2020-09-11 18:10:02 +0300

    create button for adding a patient

--- src/components/Patients/index.js
+++ src/components/Patients/index.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
+import {Button, Grid, Typography} from "@material-ui/core";
+import AddIcon from '@material-ui/icons/Add';
+
 
 const Layout = React.lazy(() => import('../../containers/Layout'))
-
+const AppTable = React.lazy(() => import('../../shared/AppTable'))
 const styles = theme => ({
-
+  button: {
+    margin: theme.spacing(1),
+  },
 })
 
-export default withStyles(styles)( 
- () => 
+export default withStyles(styles)(
+ ({classes}) =>
 <Layout>
- patients
+  <Grid
+    style={{
+      display: 'flex',
+      justifyContent:'space-between'
+    }}
+  >
+    <Button
+      variant="contained"
+      className={classes.button}
+      disableElevation
+    >
+      Registered Patients
+    </Button>
+    <Button
+      variant="contained"
+      color="primary"
+      className={classes.button}
+      startIcon={<AddIcon />}
+    >
+      Add Patient
+    </Button>
+  </Grid>
+ <AppTable/>
 </Layout>
 )
